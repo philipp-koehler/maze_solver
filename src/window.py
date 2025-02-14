b4cb5066aca812aab10363f66bf414e8a6c3408f
Philipp Koehler <philippkoehleroz@gmail.com>
2025-02-14 13:41:39 -0500

    first window created

---
+++ src/window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False 
+        
